9ff10c254cb1bc80ecb6c713bd0403d3d91f743e
Hirusha11-cpu <developer6@appletechlabs.com>
2024-10-29 17:01:59 +0530

    updated

--- app/Http/Controllers/Admin/SubCategoryController.php
+++ app/Http/Controllers/Admin/SubCategoryController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class SubCategoryController extends Controller
 {
     public function index(){
-        return view('admin.sub_category.create');
+        return view('admin.sub_category.create',compact('categories'));
     }
     public function manage(){
+        $categories = Category::all();
         return view('admin.sub_category.manage');
     }
 }
--- resources/views/admin/sub_category/create.blade.php
+++ resources/views/admin/sub_category/create.blade.php
@@ -1,7 +1,47 @@
 @extends('admin.layouts.layout')
 @section('admin_page_title')
-Create Sub Category - Admin Panel
+Create Sub Category Page
 @endsection
 @section('admin_layout')
-<h2>Create Sub Category Page</h2>
+<div class="row">
+    <div class="col-12 col-lg-6">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mb-0">Create Sub Category</h5>
+            </div>
+            @if ($errors->any())
+            <div class="alert alert-warning alert-dismissible fade show">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif 
+            @if(session('success'))
+            <div class="alert alert-success">
+                {{session('success')}}
+            </div>
+            @endif
+            <div class="card-body">
+                <form action="{{route('store.cat')}}" method="POST">
+                    @csrf
+
+                    <label for="subcategory_name" class="fw-bold md-2">Give me the category name</label>
+                    <input type="text" class="form-control" name="subcategory_name" placeholder="">
+
+                    <label for="category_id" class="fw-bold md-2">Give me the category name</label>
+                    <select name="category_id" class="form-control" id="category_id">
+                        @foreach($categories as $category)
+                        <option value="{{$category->id}}">{{$category->category_name}}</option>
+                        @endforeach
+                    </select>
+
+                    <button type="submit" class="btn btn-primary w-100">Add Category</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
